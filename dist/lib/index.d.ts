3d66001c1bd190e1a1c2eb71cef348ee25c30403
Tony <nhat.phan@ntworld.net>
2018-02-22 20:16:13 +0700

    Export intefaces

--- dist/lib/index.d.ts
+++ dist/lib/index.d.ts
@@ -1,4 +1,4 @@
-export { IFacade } from './facades/interfaces/IFacadeGrammar';
+export { IFacade, IFacadeBase, IContextualFacade, IContextualFacadeFactory, IContextualFacadeFactoryFullVerbs, IContextualFacadeVerbAt, IContextualFacadeVerbFor, IContextualFacadeVerbFrom, IContextualFacadeVerbOf, IContextualFacadeVerbWith } from './facades/interfaces/IFacadeGrammar';
 export { IFacadeContainer } from './facades/interfaces/IFacadeContainer';
 export { Facade } from './facades/Facade';
 export { ContextualFacade } from './facades/ContextualFacade';
--- lib/index.ts
+++ lib/index.ts
@@ -1,4 +1,15 @@
-export { IFacade } from './facades/interfaces/IFacadeGrammar'
+export {
+  IFacade,
+  IFacadeBase,
+  IContextualFacade,
+  IContextualFacadeFactory,
+  IContextualFacadeFactoryFullVerbs,
+  IContextualFacadeVerbAt,
+  IContextualFacadeVerbFor,
+  IContextualFacadeVerbFrom,
+  IContextualFacadeVerbOf,
+  IContextualFacadeVerbWith
+} from './facades/interfaces/IFacadeGrammar'
 export { IFacadeContainer } from './facades/interfaces/IFacadeContainer'
 export { Facade } from './facades/Facade'
 export { ContextualFacade } from './facades/ContextualFacade'
